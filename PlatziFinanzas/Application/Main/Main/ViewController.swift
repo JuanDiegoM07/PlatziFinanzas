7af19b3cb270c0129f04e3a4db206e9beac30aba
Jorge Luis Rivera Ladino <jorge.rivera@dev217.local>
2022-09-16 12:47:50 -0500

    Added project platziClass

---
+++ PlatziFinanzas/Application/Main/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Jorge Luis Rivera Ladino on 14/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PlatziFinanzas/Base/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  PlatziFinanzas
+//
+//  Created by Jorge Luis Rivera Ladino on 14/09/22.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
